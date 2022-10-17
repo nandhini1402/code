00f8182aab5aa8ea19f5a975bcdd101315118c11
nandhini1402 <nandhini.idl@gmail.com>
2022-10-17 20:53:35 +0530

    array prototye

---
+++ js/problems/8.filterMethod.js
@@ -0,0 +1,38 @@
+// const users = [
+//   { firstName: "akshay", lastName: "saini", age: 26 },
+//   { firstName: "donald", lastName: "trump", age: 75 },
+//   { firstName: "elon", lastName: "musk", age: 50 },
+//   { firstName: "deepika", lastName: "padukone", age: 26 },
+// ];
+
+// //const outPut = users.map((x) => x.firstName + " " + x.lastName);
+
+// // const outPut = users.filter((x) => {
+// //   if (x.age < 30) {
+// //     return x;
+// //   }
+// // });
+
+// //const outPut = users.filter((x) => x.age < 30);
+
+// //const outPut = users.filter((x) => x.age < 30).map((x) => x.firstName);
+
+// const outPut = users.reduce(function (res, x) {
+//   if (x.age < 30) {
+//     // var val = x.firstName;
+//     // res.push(val);
+//     // console.log(x.firstName);
+//     // res.name = x.firstName;
+//   }
+// }, {});
+
+// console.log(outPut);
+// console.log(users);
+
+Array.prototype.print = function (arr) {
+  console.log(arr);
+};
+
+function fun() {}
+
+let array = [1, 2, 3, 4, 5];
